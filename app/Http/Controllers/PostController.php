9b2eb301da248a63306d9e86b74b4f60e178210b
MAGISTER <dima8045@ukr.net>
2026-03-12 11:44:25 +0200

    Creted Post: controller, fake factory, seeder and view

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::query()
+            ->where('is_published', true)
+            ->paginate(6);
+
+        return view('posts.index', compact('posts'));
+    }
+    
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+}
---
+++ database/factories/PostFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = fake()->sentence(mt_rand(5, 8));
+        $slugBase = Str::slug($title);
+
+        $slug = $slugBase . '-' . fake()->unique()->numberBetween(1000, 999999);
+
+        return [
+            'title' => $title,
+            'slug' => $slug,
+            'expert' => fake()->paragraph(mt_rand(2, 5)),
+            'body' => collect(fake()->paragraphs(mt_rand(3, 7)))->implode("\n\n"),
+            'is_published' => fake()->boolean(70),
+            'published_at' => fake()->dateTimeBetween('-30 days', 'now'),
+            'user_id' => null
+        ];
+
+    }
+}
